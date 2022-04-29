39bf52996530bd47ae24d9f09e8f0080fa441f4b
mosama <mosama@LIN19005273.corp.capgemini.com>
2022-04-29 16:10:15 +0530

    first commit

--- jenkin/src/jenkin/Commit.java
+++ jenkin/src/jenkin/Commit.java
@@ -3,7 +3,7 @@ package jenkin;
 public class Commit {
 	public static void main(String[] args) {
 		System.out.println("this is my first project");
-		System.out.println("this is my first git");
+		System.out.println("this is my first commit");
 		System.out.println("this is my first clone");
 		System.out.println("this is my first commit");
 		System.out.println("this is my first push");
